f231e7d7aebc3eac63ca7110194f0f2ea81ad253
coolinovo <3104272@qq.com>
2020-07-21 19:20:30 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Fragment } from 'react'
+import { render } from 'react-dom'
+import App from 'views/App'
+import * as serviceWorker from './serviceWorker'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <Fragment>
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
+  </Fragment>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register()
---
+++ src/views/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'views/App.less'
+import { Button } from 'antd'
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">click!</Button>
+    </div>
+  )
+}
+
+export default App
